2f13dbb901baabca2ac90183bfe365441c5fa4f2
Ado Pabianko <ado.pabianko@efishery.com>
2024-07-10 10:28:15 +0700

    feat: add modify string

---
+++ modify-string/go.mod
@@ -0,0 +1,3 @@
+module go-labs/modify-string
+
+go 1.22.4
---
+++ modify-string/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	fmt.Println(ModifyString("o1l2le334h"))
+	fmt.Println(ModifyString("devreser"))
+	fmt.Println(ModifyString("89yek23nom"))
+}
+
+func ModifyString(str string) string {
+	strSplit := strings.Split(str, "")
+
+	var (
+		strs []string
+		re   = regexp.MustCompile(`^[0-9]+$`)
+	)
+
+	for _, v := range strSplit {
+		if !re.MatchString(v) {
+			strs = append(strs, v)
+		}
+	}
+
+	return ReverseString(strings.Join(strs, ""))
+}
+
+func ReverseString(str string) string {
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
